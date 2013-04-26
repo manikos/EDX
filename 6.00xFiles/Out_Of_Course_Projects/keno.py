f7848c7a6852273b97dddaf5f5c733ed4e936c22
manikos <mavrakis.n@gmail.com>
2013-04-26 10:59:34 +0300

    keno updated

--- 6.00xFiles/Out_Of_Course_Projects/keno.py
+++ 6.00xFiles/Out_Of_Course_Projects/keno.py
@@ -1,21 +1,123 @@
-#FINDS THE BEST NUMBER CHOICE (OUT OF 20) FOR MAXIMUM PROFIT PLAYING KENO
-import random
+#FINDS THE BEST NUMBER CHOICE (OUT OF 12) FOR MAXIMUM PROFIT PLAYING KENO
+import pylab
 
-def simpleKeno(numTrials):
-    allNumbers = range(1,81)
-    bingoList = [0.0]*20
+class simpleKeno(object):
+    """
+    Defines a class simpleKeno without take into account
+    the profits.
+    """
+    #profits per game category
+    PROFITS_TABLE = {(1,1):2.5, (1,2):1, (2,2):5, (2,3):2.5, (3,3):25,
+                     (2,4):1, (3,4):4, (4,4):100, (3,5):2, (4,5):20,
+                     (5,5):450, (3,6):1, (4,6):7, (5,6):50, (6,6):1600,
+                     (3,7):1, (4,7):3, (5,7):20, (6,7):100, (7,7):5000,
+                     (4,8):2, (5,8):10, (6,8):50, (7,8):1000, (8,8):15000,
+                     (4,9):1, (5,9):5, (6,9):25, (7,9):200, (8,9):4000,
+                     (9,9):40000, (0,10):2, (5,10):2, (6,10):20, (7,10):80,
+                     (8,10):500, (9,10):10000, (10,10):100000, (0,11):2,
+                     (5,11):1, (6,11):10, (7,11):50, (8,11):250, (9,11):1500,
+                     (10,11):15000, (11,11):500000, (0,12):4, (6,12):5,
+                     (7,12):25, (8,12):150, (9,12):1000, (10,12):2500,
+                     (11,12):25000, (12,12):1000000}
     
-    for i in range(numTrials):
-        twentyNumbers = []
-        userNumbers = []
-        while len(twentyNumbers)<20:
-            numberPC = random.choice(allNumbers)
-            if numberPC not in twentyNumbers:
-                twentyNumbers.append(numberPC)
-        while len(userNumbers)<10:
-            numberUser = random.choice(allNumbers)
-            if numberUser not in userNumbers:
-                userNumbers.append(numberUser)
-        print sorted(twentyNumbers)
-        print sorted(userNumbers)
+    def __init__(self, numbersChoice, eightyNumbers=80, twentyNumbers=20):
+        """
+        Initializes the instance with 3 parameters.
+        numbersChoice: How many numbers the user will play (1 to 12)
+        maxNum: Represents all the possible numbers. Usually is equal to 80
+        twentyNumbers: The amount of lucky numbers, usually 20
+        """
+        self.numbersChoice = numbersChoice
+        self.eightyNumbers = range(1, eightyNumbers+1)
+        self.twentyNumbers = twentyNumbers
+
+    ##START---Getter Methods
+    def getNumbersChoice(self):
+        return self.numbersChoice
+
+    def getEightyNumbers(self):
+        return self.eightyNumbers
+
+    def getTwentyNumbers(self):
+        return self.twentyNumbers
+    ###END---Getter Methods
+
+    def fillerList(self, numbersChoice):
+        """
+        Fills a list with a specific amount of numbers
+        from 1 to 80 (NOT duplicated)
+
+        numbersChoice: integer, amount of different numbers
+    
+        returns: a sorted list
+        """
+        import random
+        lista = []
+        while len(lista) < numbersChoice:
+            tempNumber = random.choice( self.getEightyNumbers() )
+            if tempNumber not in lista:
+                lista.append(tempNumber)
+        return sorted(lista)
+
+
+    def prepareGame(self):
+        """
+        Prepares the game. That is, stochastically fills
+        the lucky 20 numbers, luckyNumbers, in range [1,80]
+        and stochastically fills user's numbers, myNumbers,
+        which varies in size (1 to 12) in range [1,80].
         
+        returns: luckyNumbers (list), myNumbers (list in list)
+        """
+        self.luckyNumbers = self.fillerList(self.getTwentyNumbers())
+        self.myNumbers = []
+        for i in range(1,13):
+            self.myNumbers.append(self.fillerList(i))
+
+        return self.luckyNumbers, self.myNumbers
+
+
+    def correctMatch(self, luckyNumbers, myNumbers):
+        """
+        Checks bingo for each of the played numbers. That is,
+        if player has played 5 numbers, checks how many of these
+        numbers match the kenoNumbers, etc.
+
+        returns: a list with integers, represent successfull hits
+        """
+        correctHits = [0]*len(myNumbers)
+    
+        for i in range(len(myNumbers)):
+            for num in myNumbers[i]:
+                if num in luckyNumbers:
+                    correctHits[i] += 1
+        return correctHits #list of size 12
+        
+
+def playKeno(numTrials):
+    """
+    Checks how many sucessfull hits the player did
+    over numTrials trials
+
+    returns: avgHits, a list with the average results
+    """
+    correctHits = ( pylab.array(12) )*0
+    for i in range(numTrials):
+        keno = simpleKeno(12)
+        luckyNumbers, myNumbers = keno.prepareGame()
+        correctHits += keno.correctMatch(luckyNumbers, myNumbers)
+    avgHits = correctHits/float(numTrials)
+    
+    return avgHits
+
+
+def plotKenoResults(numTrials):
+    avgHits = playKeno(numTrials)
+    print avgHits
+    xAxis = pylab.array(range(1,13))
+    pylab.figure()
+    pylab.bar(xAxis, avgHits)
+    pylab.xlabel('User numbers choice')
+    pylab.ylabel('Successfull hits')
+    pylab.grid(True)
+    pylab.show()
